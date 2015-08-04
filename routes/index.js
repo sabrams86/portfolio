5d41769d0e31b4967a6180eafe7531fdaba69d69
Steven Abrams <steven.abrams86@gmail.com>
2015-08-04 07:17:01 -0600

    troubleshooting production site

--- routes/index.js
+++ routes/index.js
@@ -8,16 +8,16 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/send', function (req, res, next) {
-  var payload   = {
-    to      : process.env.EMAIL,
-    from    : req.body.email,
-    subject : req.body.subject,
-    text    : req.body.body
-  }
-  sendgrid.send(payload, function(err, json) {
-    if (err) { console.error(err); }
-    console.log(json);
-  });
+  // var payload   = {
+  //   to      : process.env.EMAIL,
+  //   from    : req.body.email,
+  //   subject : req.body.subject,
+  //   text    : req.body.body
+  // }
+  // sendgrid.send(payload, function(err, json) {
+  //   if (err) { console.error(err); }
+  //   console.log(json);
+  // });
   req.flash('info', 'Thank you, your message has been sent')
   res.redirect('/');
 })
